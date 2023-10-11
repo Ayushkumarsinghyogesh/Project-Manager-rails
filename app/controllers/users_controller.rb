1d2461c4a3c17d103556166bdbbb9892d03a0e75
Ayushkumarsinghyogesh <ayushkumarsinghyogesh@gmail.com>
2023-10-11 18:16:37 +0530

    New Commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
 
 
-  
+  def most_projects
+    @user = User.most_projects
+  end
 
 
   def new
@@ -14,11 +16,32 @@ class UsersController < ApplicationController
     end
   end
 
+  def create
 
-  def most_projects
-    @user = User.most_projects
+    @user = User.new(user_params)
+
+    if @user.save
+
+      session[:user_id] = @user.id
+      redirect_to projects_path
+    else
+      render :new, layout: 'sessions'
+    end
+
   end
 
+
+
+  # def show
+  #   @user = User.find_by(params[:id])
+  #
+  #   @tasks = @user.tasks
+  # end
+
+  private
+
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
 end
-
-
